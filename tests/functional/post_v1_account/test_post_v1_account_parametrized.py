7e4873f8055181876e8b8e6a0c7c167ccdab0a52
Telepnef <telepnef@gmail.com>
2026-02-09 10:04:38 +0300

    fix == test_put_v1_account_email

--- tests/functional/post_v1_account/test_post_v1_account_parametrized.py
+++ tests/functional/post_v1_account/test_post_v1_account_parametrized.py
@@ -1,10 +1,12 @@
 from json import loads
+from faker import Faker
+
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
-from faker import Faker
+
 import structlog
 import pytest
 
--- tests/functional/post_v1_account/test_put_v1_account_email.py
+++ tests/functional/post_v1_account/test_put_v1_account_email.py
@@ -1,28 +1,43 @@
 from json import loads
-
 from faker import Faker
 
 from api_mailhog.apis.mailhog_api import MailhogApi
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from tests.functional.post_v1_account.test_post_v1_account import get_activation_token_by_login
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
+
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            # sort_keys=True,
+            # separators=(',', ':')
+        )
+    ]
+)
 
 
 def test_put_v1_account_email():
-    account_api = AccountApi(host="http://185.185.143.231:5051")
-    login_api = LoginApi(host="http://185.185.143.231:5051")
-    mailhog_api = MailhogApi(host="http://185.185.143.231:5025")
+    dm_api_configuration = DmApiConfiguration(host="http://185.185.143.231:5051", disable_logs=False)
+    mailhog_configuration = MailhogConfiguration(host="http://185.185.143.231:5025", disable_logs=True)
+
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mailhog_api = MailhogApi(configuration=mailhog_configuration)
 
     faker = Faker()
 
     login = faker.name().replace(" ", "")
     email = f"{login}@mail.com"
     password = "12345678"
-
     new_email = f"updated_{email}"
 
     # Регистрируемся
-
     json_data = {
         'login': login,
         'email': email,
@@ -30,20 +45,16 @@ def test_put_v1_account_email():
     }
 
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
     assert response.status_code == 201, f"Пользователь не был создан {response.text}"
 
     # Получаем активационный токен и активируем
-
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
     assert response.status_code == 200, "Письмо не получено"
 
     token = get_activation_token_by_login(login, response)
     assert token is not None, f"Токен для пользователя {login} не был получен"
 
     response = account_api.put_v1_account_token(token=token)
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не активирован"
 
     # Заходим
@@ -54,12 +65,9 @@ def test_put_v1_account_email():
     }
 
     response = login_api.post_v1_account_login(json_data=json_data)
-
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не авторизован"
 
     # Меняем емейл
-
     json_data = {
         "login": login,
         "password": password,
@@ -67,12 +75,9 @@ def test_put_v1_account_email():
     }
 
     response = account_api.put_v1_account_change_mail(json_data=json_data)
-
-    print(response.status_code)
     assert response.status_code == 200, f"Пользователю {login} не удалось изменить почту"
 
     # Пытаемся войти, получаем 403
-
     json_data = {
         'login': login,
         'password': password,
@@ -80,28 +85,20 @@ def test_put_v1_account_email():
     }
 
     response = login_api.post_v1_account_login(json_data=json_data)
-
-    print(response.status_code)
     assert response.status_code == 403, "Ожидалась ошибка 403, т.к. пользователь не подтвердил смену почты"
 
     # На почте находим токен по новому емейлу для подтверждения смены емейла
-
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
     assert response.status_code == 200, "Письмо не получено"
 
     token = get_activation_token_by_email(new_email, response)
     assert token is not None, f"Токен для пользователя {login} не был получен"
 
     # Активируем этот токен
-
     response = account_api.put_v1_account_token(token=token)
-
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не активирован"
 
     # Логинимся
-
     json_data = {
         'login': login,
         'password': password,
@@ -109,8 +106,6 @@ def test_put_v1_account_email():
     }
 
     response = login_api.post_v1_account_login(json_data=json_data)
-
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не авторизован"
 
 
