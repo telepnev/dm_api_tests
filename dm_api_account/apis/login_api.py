ee71a47653b9f54a5532a0ff181e3d9587edb50a
Telepnef <telepnef@gmail.com>
2026-02-09 09:08:02 +0300

    добавлен RestClient и curlify

--- dm_api_account/apis/login_api.py
+++ dm_api_account/apis/login_api.py
@@ -1,10 +1,9 @@
 import requests
 
+from restclient.client import RestClient
 
-class LoginApi:
-    def __init__(self, host, headers=None):
-        self.host = host
-        self.headers = headers
+
+class LoginApi(RestClient):
 
     def post_v1_account_login(self, json_data):
         """
@@ -12,8 +11,8 @@ class LoginApi:
         :param json_data:
         :return:
         """
-        response = requests.post(
-            url=f'{self.host}/v1/account/login',
+        response = self.post(
+            path=f'/v1/account/login',
             json=json_data
         )
         return response
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -4,6 +4,18 @@ from faker import Faker
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            # sort_keys=True,
+            # separators=(',', ':')
+        )
+    ]
+)
 
 
 def test_post_v1_account():
@@ -12,11 +24,12 @@ def test_post_v1_account():
     mailhog_api = MailhogApi(host="http://185.185.143.231:5025")
 
     faker = Faker()
-    login = faker.name().replace(" ", "")
-    email = f"{login}@mail.com"
+    # login = faker.name().replace(" ", "")
+    # email = f"{login}@mail.com"
+    login = "evgen123"
+    email = "evgen123@email.ru"
+
     password = "12345678"
-
-
 
     # Регистрация пользователя
     json_data = {
@@ -26,12 +39,10 @@ def test_post_v1_account():
     }
 
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
     assert response.status_code == 201, f"Пользователь не был создан {response.text}"
 
     # Получить письма из почтового ящика
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
     assert response.status_code == 200, "Письмо не получено"
 
     # Получить активационный токен
@@ -43,7 +54,6 @@ def test_post_v1_account():
     # Активация пользователя
     response = account_api.put_v1_account_token(token=token)
 
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не активирован"
 
     # Авторизация
@@ -55,7 +65,6 @@ def test_post_v1_account():
 
     response = login_api.post_v1_account_login(json_data=json_data)
 
-    print(response.status_code)
     assert response.status_code == 200, "Пользователь не авторизован"
 
 
