668a7e96c83d9d39a51861870ba601d3d99ec195
Telepnef <telepnef@gmail.com>
2026-02-12 11:01:02 +0300

    добавил фикстуры в тест test_post_v1_account

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+import pytest
 import structlog
 from faker import Faker
 
@@ -18,26 +21,44 @@ structlog.configure(
     ]
 )
 
-
-def test_post_v1_account():
-    dm_api_configuration = DmApiConfiguration(host="http://185.185.143.231:5051", disable_logs=False)
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host="http://185.185.143.231:5025", disable_logs=True)
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
 
+@pytest.fixture
+def account_api():
+    dm_api_configuration = DmApiConfiguration(host="http://185.185.143.231:5051", disable_logs=False)
     account = DmApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
+    return account
 
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+@pytest.fixture
+def account_helper(account_api, mailhog_api):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
 
+@pytest.fixture
+def prepare_user():
     faker = Faker()
     login = faker.name().replace(" ", "")
     email = f"{login}@mail.com"
     password = "12345678"
 
+    # создаем юзера через namedtuple, быстрый питанячий класс
+    User = namedtuple("User", ["login", "email", "password"])
+    user = User(login=login, email=email, password=password)
+    return user
+
+
+def test_post_v1_account(account_helper,prepare_user):
+
+
     account_helper.register_new_user(
-        login=login,
-        email=email,
-        password=password
+        login=prepare_user.login,
+        email=prepare_user.email,
+        password=prepare_user.password
     )
     account_helper.user_login(
-        login=login,
-        password=password)
+        login=prepare_user.login,
+        password=prepare_user.password)
